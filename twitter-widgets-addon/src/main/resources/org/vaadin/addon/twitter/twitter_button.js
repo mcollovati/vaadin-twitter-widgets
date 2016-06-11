1fe1fa2df3b95b5ef9aa1f426b619b67c92c765a
Marco Collovati <mcollovati@gmail.com>
2016-06-11 23:30:32 +0200

    text preview for buttons

--- twitter-widgets-addon/src/main/resources/org/vaadin/addon/twitter/twitter_button.js
+++ twitter-widgets-addon/src/main/resources/org/vaadin/addon/twitter/twitter_button.js
@@ -1,11 +1,33 @@
 window.org_vaadin_addon_twitter_TweetButton = function() {
     var me = this;
+
+
+
+    var createPreviewElement = function() {
+
+        var previewTextMapper = {
+            follow: function(state) { return "Follow @" + state.primaryArgument; },
+            share: function(state) { return "Tweet"; },
+            mention: function(state) { return "Tweet @" + state.primaryArgument; },
+            hashtag: function(state) { return "Tweet #" + state.primaryArgument; },
+        }
+
+        var buttonType = me.getState().buttonType.toLowerCase();
+        var el = document.createElement("A");
+        el.className = "twitter-" + buttonType + "-button";
+        if (buttonType === "follow") {
+            el.href = "https://twitter.com/" + me.getState().primaryArgument;
+        } else {
+            el.href = "https://twitter.com/intent/tweet";
+        }
+        el.innerText = previewTextMapper[buttonType](me.getState());
+        return el;
+    }
     var optsNames = ["align", "count", "size", "text",
                      { key: "showScreenName", map: function(val) { return (val === false) ? false : true }}];
 
     var createOptions = function(state) {
         var opts = window.vaadinTwttr.mergeOptions(state, optsNames);
-        console.log("=============== OPTS", opts);
         return opts;
     }
 
@@ -15,10 +37,15 @@ window.org_vaadin_addon_twitter_TweetButton = function() {
         window.twttr.widgets[generatorFn](state.primaryArgument, me.getElement(), createOptions(state))
         .then(function (el) {
             // TODO: callback to java component?
+            me.getElement().removeChild(me.previewElement);
+            delete me.previewElement;
             console.log(state.buttonType + " button create with opts")
         });
     }
 
+
+    me.previewElement = createPreviewElement();
+    me.getElement().appendChild(me.previewElement);
     me.onStateChange = function() {
         window.twttr.ready(createButton);
     };
