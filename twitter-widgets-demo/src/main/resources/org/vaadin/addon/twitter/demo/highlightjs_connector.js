ddfbdd7e592ef469568decde47f2a698118da557
Marco Collovati <mcollovati@gmail.com>
2018-07-16 18:23:05 +0200

    Clean up and header update for release

--- twitter-widgets-demo/src/main/resources/org/vaadin/addon/twitter/demo/highlightjs_connector.js
+++
@@ -1,9 +0,0 @@
-window.org_vaadin_addon_twitter_demo_HighlightJS = function() {
-    var me = this;
-    me.onStateChange = function() {
-        if (hljs) {
-            hljs.highlightBlock(me.getElement(me.getParentId()).querySelector("pre code"));
-        }
-    };
-
-};
