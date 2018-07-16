5cd4b67f55403bcfb5ef71f89577eee0ebc17554
Marco Collovati <mcollovati@gmail.com>
2018-07-16 18:11:48 +0200

    Added more options to dynamic demo

--- twitter-widgets-demo/src/main/java/org/vaadin/addon/twitter/demo/DynamicDemo.java
+++ twitter-widgets-demo/src/main/java/org/vaadin/addon/twitter/demo/DynamicDemo.java
@@ -1,5 +1,6 @@
 package org.vaadin.addon.twitter.demo;
 
+import java.util.EnumSet;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 
@@ -9,7 +10,9 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.vaadin.addon.twitter.Alignment;
 import org.vaadin.addon.twitter.Tweet;
 
 @Route(value = "dynamic", layout = DemoUI.class)
@@ -21,25 +24,48 @@ public class DynamicDemo extends Composite<HorizontalLayout> {
         tweet = new Tweet("763309653497446401");
         tweet.addClassNames("tw-widget", "tw-single-tweet");
         FormLayout optionsForm = new FormLayout();
+        optionsForm.setSizeUndefined();
+
         buildOptions(optionsForm);
         getContent().add(optionsForm);
         getContent().setAlignSelf(FlexComponent.Alignment.CENTER, tweet);
         getContent().add(tweet);
-        getContent().expand(tweet);
+        getContent().setFlexGrow(0, optionsForm);
+        getContent().setFlexGrow(1, tweet);
     }
 
     private void buildOptions(FormLayout optionsForm) {
 
-        ComboBox<Tweet.Theme> theme = new ComboBox<>();
-        theme.setRequired(true);
-        theme.setPreventInvalidInput(true);
-        theme.setAllowCustomValue(false);
-        theme.setItems(Tweet.Theme.values());
-        theme.setValue(tweet.getTheme());
-        theme.addValueChangeListener(event -> applyOnTweet(event, Tweet::withTheme));
+        ComboBox<Alignment> alignment = buildCombobox(tweet.getAlign(), Tweet::withAlign);
+        optionsForm.addFormItem(alignment, "Alignment");
 
+        ComboBox<Tweet.Theme> theme = buildCombobox(tweet.getTheme(), Tweet::withTheme);
         optionsForm.addFormItem(theme, "Theme");
 
+        ComboBox<Tweet.Cards> cards = buildCombobox(tweet.getCards(), Tweet::withCards);
+        optionsForm.addFormItem(cards, "Cards");
+
+        ComboBox<Tweet.Conversation> conversation = buildCombobox(tweet.getConversation(), Tweet::withConversation);
+        optionsForm.addFormItem(conversation, "Conversation");
+
+        TextField linkColor = new TextField();
+        linkColor.setPlaceholder("Color in HTML format (#FF00FF)");
+        linkColor.addValueChangeListener(event -> tweet.withLinkColor(event.getValue()));
+        optionsForm.addFormItem(linkColor, "Link color");
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends Enum<T>> ComboBox<T> buildCombobox(T defValue, BiConsumer<Tweet, T> setter) {
+
+        ComboBox<T> comboBox = new ComboBox<>();
+        comboBox.setRequired(true);
+        comboBox.setPreventInvalidInput(true);
+        comboBox.setAllowCustomValue(false);
+        comboBox.setItems(EnumSet.<T>allOf((Class<T>)defValue.getClass()));
+        comboBox.setValue(defValue);
+        comboBox.addValueChangeListener(event -> applyOnTweet(event, setter));
+        return comboBox;
     }
 
     private <T> void applyOnTweet(HasValue.ValueChangeEvent<T> event, BiConsumer<Tweet, T> action) {
